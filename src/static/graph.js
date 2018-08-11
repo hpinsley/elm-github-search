5b7f6804e1bff4387ff50356154ef5787facd1a3
Howard Pinsley <hpinsley@gmail.com>
2018-08-10 20:25:23 -0400

    Pretty cool

--- src/static/graph.js
+++ src/static/graph.js
@@ -6,7 +6,7 @@ function renderGraph(title) {
 
     var rootElement = $('#graphContent');
     rootElement.append('<h1>This is from JavaScript</h1>');
-    
+
     var sampleSVG = d3.select("#graphContent")
         .append("svg")
         .attr("width", 100)
@@ -20,6 +20,19 @@ function renderGraph(title) {
         .attr("cy", 50)
         .on("mouseover", function(){d3.select(this).style("fill", "aliceblue");})
         .on("mouseout", function(){d3.select(this).style("fill", "white");});
+
+    sampleSVG.append("circle")
+        .style("stroke", "gray")
+        .style("fill", "white")
+        .attr("r", 40)
+        .attr("cx", 50)
+        .attr("cy", 50)
+      .transition()
+        .delay(100)
+        .duration(1000)
+        .attr("r", 10)
+        .attr("cx", 30)
+        .style("fill", "black");
 }
 
 export { renderGraph };
--- src/static/index.js
+++ src/static/index.js
@@ -1,19 +1,9 @@
-// pull in desired CSS/SASS files
 require( './styles/main.scss' );
-// import {$,jQuery} from 'jquery';
-// // export for others scripts to use
-// window.$ = $;
-// window.jQuery = jQuery;
-
-// var $ = jQuery = require( '../../node_modules/jquery/dist/jquery.js' );           // <--- remove if jQuery not needed
-// require( '../../node_modules/bootstrap-sass/assets/javascripts/bootstrap.js' );   // <--- remove if Bootstrap's JS not needed
 import {renderGraph} from './graph'
 
 // inject bundled Elm app into div#main
 var Elm = require( '../elm/Main' );
 var app = Elm.Main.embed( document.getElementById( 'main' ) );
-console.log(app)
-console.log(app.ports)
 
 app.ports.setFocusToElement.subscribe(({id, delay}) => {
     // We need to wait a bit to allow the DOM to be rendered...
@@ -28,6 +18,5 @@ app.ports.setFocusToElement.subscribe(({id, delay}) => {
 });
 
 app.ports.render.subscribe(({title}) => {
-    console.log(`Rendering graph with title ${title}.`);
     renderGraph(title);
 });
